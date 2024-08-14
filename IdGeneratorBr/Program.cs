8eaebdea999991e0cf2c5afd189b3881c80e5a93
matheus_falcao <matheusfalcao695@gmail.com>
2024-08-13 21:26:21 -0300

    First Commit

---
+++ IdGeneratorBr/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using IdGeneratorBr.Services;
+using TextCopy;
+
+GeneratorService generator = new GeneratorService();
+
+bool start = true;
+
+Console.WriteLine("Gerador de documentos BR");
+
+while (start)
+{
+    Console.WriteLine("1 - Gerar CPF");
+
+    string? decision = Console.ReadLine();
+    Console.WriteLine(" ");
+
+    while (decision == null || decision != "1")
+    {
+        Console.WriteLine("Insira um valor válido.");
+
+        Console.WriteLine("1 - Gerar CPF");
+
+        decision = Console.ReadLine();
+        Console.WriteLine(" ");
+    }
+
+    if (decision == "1")
+    {
+        var cpf = generator.GenerateCpf();
+        Console.WriteLine($"{cpf}");
+        ClipboardService.SetText(cpf);
+        Console.WriteLine("CPF copiado para area de transferência.");
+    }
+
+    Console.WriteLine("Deseja gerar um novo documento?");
+    decision = Console.ReadLine();
+
+    if (decision == "1")
+    {
+        start = true;
+    }
+    else
+    {
+        start = false;
+    }
+}
---
+++ IdGeneratorBr/Services/GeneratorService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdGeneratorBr.Services
+{
+    public class GeneratorService
+    {
+        public string GenerateCpf()
+        {
+            Random rnd = new Random();
+            int firstDigit;
+            int lastDigit;
+
+            var cpf = rnd.Next(100000000, 999999999).ToString();
+            List<int> cpfMembers = cpf.Select(digit => int.Parse(digit.ToString())).ToList();
+            firstDigit = GenerateVerifyDigitsCpf(cpfMembers, 10);
+
+            cpfMembers.Add(firstDigit);
+
+            lastDigit = GenerateVerifyDigitsCpf(cpfMembers, 11);
+
+            return cpf + firstDigit + lastDigit;
+        }
+    
+        private int GenerateVerifyDigitsCpf(List<int> cpfMembers, int mulplier)
+        {
+            int sum = 0;
+            int division;
+            int rest;
+
+            foreach (int position in cpfMembers)
+            {
+                sum += (position * mulplier);
+
+                mulplier--;
+            }
+
+            division = sum / 11;
+            rest = sum - (11 * division);
+
+            switch (rest)
+            {
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                case 10:
+                    return 11 - rest;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
